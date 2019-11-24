da267ca95737ac7b8ea030c9e08c7600a533e944
sandi <suransandi@gmail.com>
2019-11-24 23:23:10 +0100

    RideCreator component update

--- ClientApp/src/containers/RideCreator/RideCreator.js
+++ ClientApp/src/containers/RideCreator/RideCreator.js
@@ -1,48 +1,152 @@
-import React, { Component } from 'react';
-import Aux from '../../hoc/Aux/Aux';
+import React, { Component } from "react";
+import axios from "axios";
+import Aux from "../../hoc/Aux/Aux";
+import DatePicker from "react-datepicker";
+import Select from "react-select";
+
+import classes from "./RideCreator.module.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 class RideCreator extends Component {
-    state = {
-        cars: [],
-        employees:[], 
-        loading: true
-    }
+  state = {
+    cars: [],
+    employees: [],
+    multiOptions: [],
+    selectedCar: {
+      name: "",
+      licensePlate: ""
+    },
+    selectedEmployees: [],
+    startLocation: "",
+    endLocation: "",
+    startDate: new Date(),
+    endDate: new Date(),
+    isValidRide: true,
+    loading: true
+  };
 
-    componentDidMount() {
-        this.populateCarData();
-        this.populateEmployeeData();
-    }
+  componentDidMount() {
+    const requestCars = axios.get("car");
+    const requestEmployees = axios.get("employee");
+    axios
+      .all([requestCars, requestEmployees])
+      .then(
+        axios.spread((...responses) => {
+          const multiOptions = responses[1].data.map(emp => {
+            return { value: emp.id, label: emp.fullName };
+          });
+          this.setState({
+            cars: responses[0].data,
+            employees: responses[1].data,
+            multiOptions: multiOptions,
+            loading: false
+          });
+        })
+      )
+      .catch(errors => {
+        console.error(errors);
+      });
+  }
+  postDataHandler = () => {
+    let selectedEmpIds = [];
+    this.state.selectedEmployees.forEach(empl => {
+      selectedEmpIds.push(empl.value);
+    });
+    const data = {
+      startLocation: this.state.startLocation,
+      endLocation: this.state.endLocation,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+      carLicensePlate: this.state.selectedCar.licensePlate,
+      employeeIdList: selectedEmpIds
+    };
+    axios.post("travel", data).then(response => {
+      console.log(response);
+    });
+  };
 
-    static renderRideCreator() {
-        return (
-            <h1 id="tableLabel" >New Travels</h1>
-        )
-    }
-       
+  startLocationChangeHandler = event => {
+    this.setState({ startLocation: event.target.value });
+  };
+  endLocationChangeHandler = event => {
+    this.setState({ endLocation: event.target.value });
+  };
+  startDateChangeHandler = date => {
+    this.setState({ startDate: date });
+  };
+  endDateChangeHandler = date => {
+    this.setState({ endDate: date });
+  };
+  handleMultiSelectChange = selectedOption => {
+    this.setState({ selectedEmployees: selectedOption });
+  };
 
-    render(){
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : RideCreator.renderRideCreator();
-        return(
-            <Aux>
-                   {contents}       
-            </Aux>
-        );
-        
-    }
+  render() {
+    const {
+      startLocation,
+      endLocation,
+      startDate,
+      endDate,
+      selectedEmployees,
+      multiOptions
+    } = this.state;
 
-    async populateCarData() {
-        const response = await fetch('car');
-        const data = await response.json();
-        this.setState({ cars: data, loading: false });
-    }
-
-    async populateEmployeeData() {
-        const response = await fetch('employee');
-        const data = await response.json();
-        this.setState({ employees: data, loading: false });
-    }
+    return (
+      <Aux>
+        <div className={classes.NewRide}>
+          <h1>New Ride</h1>
+          <label>Start Location</label>
+          <input
+            type="text"
+            value={startLocation}
+            onChange={this.startLocationChangeHandler}
+          />
+          <label>End Location</label>
+          <input
+            type="text"
+            value={endLocation}
+            onChange={this.endLocationChangeHandler}
+          />
+          <label>Start Date</label>
+          <DatePicker
+            selected={startDate}
+            onChange={this.startDateChangeHandler}
+          />
+          <label>End Date</label>
+          <DatePicker selected={endDate} onChange={this.endDateChangeHandler} />
+          <label>Choose Employees for Ride</label>
+          <Select
+            value={selectedEmployees}
+            onChange={this.handleMultiSelectChange}
+            closeMenuOnSelect={false}
+            options={multiOptions}
+            isMulti
+          />
+          <label>Car</label>
+          <select
+            value={this.state.selectedCar.licensePlate}
+            onChange={event =>
+              this.setState({
+                selectedCar: {
+                  name: event.target.selectedOptions[0].text,
+                  licensePlate: event.target.value
+                }
+              })
+            }
+          >
+            {this.state.cars.map(car => {
+              return (
+                <option key={car.licensePlate} value={car.licensePlate}>
+                  {car.name}
+                </option>
+              );
+            })}
+          </select>
+          <button onClick={this.postDataHandler}>Create a carpool ride</button>
+        </div>
+      </Aux>
+    );
+  }
 }
 
 export default RideCreator;
